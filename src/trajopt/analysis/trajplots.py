25f3a8620f7367bdb8801a4657df14ffb8c39e99
Daniel Calderone <dan@Daniels-MacBook-Air-109.local>
2025-11-15 12:24:11 -0700

    more plots

--- src/trajopt/analysis/trajplots.py
+++ src/trajopt/analysis/trajplots.py
@@ -16,19 +16,40 @@ class SCVXPLOTS:
         self.base_pen = {**self.base_pen,'msty':'','msz':1}
         self.legends = {};
 
+        self.use_current = False;
+        self.current_scenarios = [];
+        self.current_methods = [];
+        self.current_runs = [];
+        self.current_iters = [];
+
+    def setCurrent(self,ins={}):
+        self.use_current = True
+        if 'methods' in ins: self.current_methods = ins['methods'];
+        if 'scenarios' in ins: self.current_scenarios = ins['scenarios'];
+        if 'runs' in ins: self.current_runs = ins['runs'];
+        if 'iters' in ins: self.current_iters = ins['iters'];
 
 
     ########### BASIC 2D-PLOTTING ###############
     def addPlot2D(self,ax,pen={},typ='line',ins={}):
         if len(pen)==0: penn = self.base_pen.copy();
         else: penn = {**self.base_pen,**pen}
-        scenario = self.scenarios[0];
-        if 'scenario' in ins: scenario = ins['scenario'];
-        method = self.methods[scenario][0];
-        if 'method' in ins: method = ins['method'];
-        runs = [0]; iters = [0];
+
+        scenarios = [self.scenarios[0]];
+        methods = [self.methods[scenarios[0]][0]];
+        runs = [0];
+        iters = [0];
+        if self.use_current:
+            scenarios = self.current_scenarios;
+            methods = self.current_methods;
+            runs = self.current_runs;
+            iters = self.current_iters;
+        if 'scenarios' in ins: scenarios = ins['scenarios'];
+        if 'methods' in ins: methods = ins['methods'];
         if 'runs' in ins: runs = ins['runs'];
         if 'iters' in ins: iters = ins['iters'];
+
+
         xtag = None; ytag = None;
         if 'x' in ins: xtag = ins['x'];
         if 'y' in ins: ytag = ins['y'];
@@ -38,37 +59,49 @@ class SCVXPLOTS:
         label = 'blarg';
         if 'label' in ins: label = ins['label']
 
-        for r in runs:
-            RUNS = self.data[scenario][method]['mc_data'];
-            if r < len(RUNS):
-                RUN = RUNS[r];
-                for i in iters:
-                    if i < len(RUN['iters']):
-                        data = RUN['iters'][i];
-                        if not(ytag == None):
-                            if isinstance(ytag,tuple): ydata = data[ytag[0]][:,ytag[1]];
-                            else: ydata = data[ytag];
-                            if xtag == None: xdata = list(range(len(ydata)));
-                            else:
-                                if isinstance(xtag,tuple): xdata = data[xtag[0]][:,xtag[1]];
-                                else: xdata = data[xtag];
+        for scenario in scenarios:
+            for method in methods:
+                if method in self.data[scenario]:
+                    RUNS = self.data[scenario][method]['mc_data'];
+                    for r in runs:
+                        if r < len(RUNS):
+                            RUN = RUNS[r];
+                            for i in iters:
+                                if i < len(RUN['iters']):
+                                    data = RUN['iters'][i];
+                                    if not(ytag == None):
+                                        if isinstance(ytag,tuple): ydata = data[ytag[0]][:,ytag[1]];
+                                        else: ydata = data[ytag];
+                                        if xtag == None: xdata = list(range(len(ydata)));
+                                        else:
+                                            if isinstance(xtag,tuple): xdata = data[xtag[0]][:,xtag[1]];
+                                            else: xdata = data[xtag];
 
-                            frgba = penn['frgba']; lrgba = penn['lrgba'];
-                            lw = penn['lw']; ls = penn['ls']
-                            msty = penn['msty']; msz = penn['msz']
-                            if typ == 'line':
-                                if leg == None: 
-                                    ax.plot(xdata,ydata.T,color=lrgba[:3],alpha=lrgba[3],linewidth=lw,linestyle = ls,marker=msty,markersize=msz)
-                                else: 
-                                    self.legends[leg][label] = ax.plot(xdata,ydata.T,
-                                        label=label,color=lrgba[:3],alpha=lrgba[3],
-                                        linewidth=lw,linestyle = ls,marker=msty,markersize=msz)[0]
+                                        frgba = penn['frgba']; lrgba = penn['lrgba'];
+                                        lw = penn['lw']; ls = penn['ls']
+                                        msty = penn['msty']; msz = penn['msz']
+                                        if typ == 'line':
+                                            if leg == None: 
+                                                ax.plot(xdata,ydata.T,color=lrgba[:3],alpha=lrgba[3],linewidth=lw,linestyle = ls,marker=msty,markersize=msz)
+                                            else: 
+                                                self.legends[leg][label] = ax.plot(xdata,ydata.T,
+                                                    label=label,color=lrgba[:3],alpha=lrgba[3],
+                                                    linewidth=lw,linestyle = ls,marker=msty,markersize=msz)[0]
+
+
+    def setParams(self,ax,ins={}):
+        if 'ticks' in ins: ax.tick_params(**ins['ticks'])
+        if 'xticks' in ins: ax.tick_params(**ins['xticks'])
+        if 'yticks' in ins: ax.tick_params(**ins['yticks'])
+        if 'xlabel' in ins: temp = ins['xlabel']; label = temp['label']; temp.pop('label'); ax.set_xlabel(label,**temp)
+        if 'ylabel' in ins: temp = ins['ylabel']; label = temp['label']; temp.pop('label'); ax.set_ylabel(label,**temp)
+        if 'title' in ins: temp = ins['title']; title = temp['text']; temp.pop('text'); ax.set_title(title,**temp);
 
 
     ######## LABELS AND LEGENDS ############
     def setTicks(self,ax,x=False,y=False,ins={}):
-        if x==True:  ax.tick_params(**ins)
-        if y==True:  ax.tick_params(**ins)
+        if x==True:  ax.tick_params(**ins);
+        if y==True:  ax.tick_params(**ins);
 
     def setLabels(self,ax,xlabel='',ylabel='',ins={}):
         ax.set_xlabel(xlabel,**ins)
