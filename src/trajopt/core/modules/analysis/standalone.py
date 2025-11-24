ba6a30e83697bd27b65769ad04a8bf0582e6d7a3
Carlos Morales <carlosm3@uw.edu>
2025-11-23 21:45:03 -0600

    standalone thingy

--- src/trajopt/core/modules/analysis/standalone.py
+++ src/trajopt/core/modules/analysis/standalone.py
@@ -1,9 +1,9 @@
 import trajopt.analysis.default_analysis as default_analysis
 
-def run_standalone_analysis(problem):
+def run_standalone_analysis(problem, method_name):
     # populate scenario_data dict for plotting
     scenario_data = {
-        "method": {
+        method_name: {
             "mc_data": [default_analysis.perform_default_analysis(problem)]
         }
     }
