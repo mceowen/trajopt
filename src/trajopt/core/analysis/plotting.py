2614d2ccfacdc0c59dacf116873222c90401963e
Carlos Morales <carlosm3@uw.edu>
2026-02-06 11:04:00 -0800

    remove latex requirement

--- src/trajopt/core/analysis/plotting.py
+++ src/trajopt/core/analysis/plotting.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from trajopt.core.analysis.trajplots import SCVXPLOTS
+plt.rcParams["text.usetex"] = False
 
 # =============================================================================
 # Pen Styles
