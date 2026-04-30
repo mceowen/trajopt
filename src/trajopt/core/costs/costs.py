a7ae2980648f9c7cebd33c39dbd8ac54b7d8964c
pramasahayam <pranav.rr@icloud.com>
2026-04-30 14:19:19 -0700

    Fixed formatting in costs.py, docstrings already present.

--- src/trajopt/core/costs/costs.py
+++ src/trajopt/core/costs/costs.py
@@ -1,6 +1,8 @@
-import trajopt.core.costs.costs_library as costs_library
 import inspect
 from functools import partial
+
+from trajopt.core.costs import costs_library
+
 
 class Costs:
     def __init__(self, config, index_map, fcns=None):
@@ -15,8 +17,7 @@ class Costs:
             self.register_cost(cost_config_i, index_map, fcns=fcns)
 
     def register_cost(self, cost_config, index_map, fcns=None):
-        """
-        Register a cost object in the costs list given a cost configuration.
+        """Register a cost object in the costs list given a cost configuration.
 
         Args:
             cost_config: Cost configuration dictionary.
@@ -25,81 +26,82 @@ class Costs:
 
         Returns:
             None.
+
         """
         cost_type = cost_config["type"]
         costClass = getattr(costs_library, cost_type)
         self.costs_list.append(costClass(cost_config, index_map, fcns=fcns))
 
     def get(self, **kwargs):
-        """
-        Get all costs that match given keyword arguments.
+        """Get all costs that match given keyword arguments.
 
         Args:
             **kwargs: Keyword arguments to match against cost attributes.
 
         Returns:
             List of costs that match the given keyword arguments.
+
         """
         selected_costs = [cost for cost in self.costs_list if all(getattr(cost, k, None) == v for k, v in kwargs.items())]
         return selected_costs
 
     def has(self, **kwargs):
-        """
-        Check if any costs match given keyword arguments.
+        """Check if any costs match given keyword arguments.
 
         Args:
             **kwargs: Keyword arguments to match against cost attributes.
 
         Returns:
             True if any costs match all given keyword arguments, False otherwise.
+
         """
         return any(all(getattr(cost, k, None) == v for k, v in kwargs.items()) for cost in self.costs_list)
 
     def resolve_functions(self, fcns):
-        """
-        Bind user-provided functions to cost objects and wrap 'fcns' dictionary.
+        """Bind user-provided functions to cost objects and wrap 'fcns' dictionary.
 
         Args:
             fcns: Dictionary of user-provided functions.
 
         Returns:
             None.
+
         """
         for cost in self.costs_list:
-            if getattr(cost, 'fcn_dim', None) is not None:
+            if getattr(cost, "fcn_dim", None) is not None:
                 sig = inspect.signature(cost.fcn_dim)
                 param_names = sig.parameters.keys()
 
                 kwargs_to_bind = {}
-                if 'fcns' in param_names:
+                if "fcns" in param_names:
                     kwargs_to_bind = {"fcns": fcns}
 
                 if kwargs_to_bind:
                     cost.fcn_dim = partial(cost.fcn_dim, **kwargs_to_bind)
 
     def nondim_costs(self, nondim):
-        """
-        Non-dimensionalize all costs.
+        """Non-dimensionalize all costs.
 
         Args:
             nondim: Non-dimensionalization object.
 
         Returns:
             None.
+
         """
         for cost in self.costs_list:
             cost.nondim_cost(nondim)
 
     def convexify_costs(self):
-        """
-        Convexify all costs. If a cost has a 'convexify_cost' method, call it.
+        """Convexify all costs. If a cost has a 'convexify_cost' method, call it.
 
         Args:
             None.
 
         Returns:
             None.
+
         """
         for cost in self.costs_list:
-            if getattr(cost, 'convexify_cost', None) is not None:
+            if getattr(cost, "convexify_cost", None) is not None:
                 cost.convexify_cost()
