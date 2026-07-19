da2083aeca7bbb5a9f9c4d21aff3aeff05dba6fa
Skye Mceowen <skye@mceowen.io>
2026-07-18 22:50:24 -0700

    updated second order regularization

--- src/trajopt/methods/scp/scp_segment.py
+++ src/trajopt/methods/scp/scp_segment.py
@@ -81,7 +81,7 @@ class SCPSegment():
         initial_guess.set_initial_guess(segment, self)
 
         self.iter_data_list = []
-        self.lm_mu = 1e-8
+        self.lm_mu = 0.0 #1e-8
 
         self.current_iter_data = recursive_attrdict({
             "iter_num": 0,
@@ -310,7 +310,6 @@ class SCPSegment():
             H = np.tile(np.eye(n_z + n_nu), (N, 1, 1))
         else:
             H = np.zeros((N, n_z + n_nu, n_z + n_nu))
-            self._adapt_levenberg(iteration)
             H += self.lm_mu * np.eye(n_z + n_nu)[np.newaxis, :, :]
 
         for constraint in self.constraints.values():
@@ -320,21 +319,6 @@ class SCPSegment():
             cost.accumulate_hessian(self, H)
 
         self.cp_params.L.value = _psd_sqrt(H)
-
-    def _adapt_levenberg(self, iteration: int) -> None:
-        if iteration < 2:
-            return
-        prev = self.iter_data_list[-1]
-        prev_prev = self.iter_data_list[-2]
-        merit_curr = prev.cost + prev.penalty_cost
-        merit_prev = prev_prev.cost + prev_prev.penalty_cost
-        ratio = merit_curr / merit_prev if merit_prev != 0 else 1.0
-        if ratio > 1.0:
-            self.lm_mu = min(self.lm_mu * 3.0, 1e4)
-        elif ratio > 0.99:
-            self.lm_mu = min(self.lm_mu * 1.5, 1e4)
-        else:
-            self.lm_mu = max(self.lm_mu * 0.7, 1e-6)
 
     def read_solution(self) -> None:
         self._dz_new  = self.dz.value
@@ -396,6 +380,6 @@ class SCPSegment():
 
 def _psd_sqrt(H_batch):
     eigvals, eigvecs = np.linalg.eigh(H_batch)
-    eigvals_reg = np.maximum(eigvals, 0.0)
+    eigvals_reg = np.maximum(eigvals, 1e-6)
     sqrt_eigvals = np.sqrt(eigvals_reg)
     return sqrt_eigvals[..., :, np.newaxis] * np.transpose(eigvecs, (0, 2, 1))
